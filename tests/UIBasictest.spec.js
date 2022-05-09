0593298d45c9a4b06cef40385712a71f55680d9d
DucAnhNguyen <ducanhn1308@gmail.com>
2022-05-09 17:23:10 +0700

    setup core concept

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,14 +1,22 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
 test("Browser Context PlayWRight Test", async ({ browser }) => {
   // Create a new incognito browser context.
   const context = await browser.newContext();
   // Create a new page in a pristine context.
   const page = await context.newPage();
-  //   await page.goto("https://example.com");
-  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await page.goto(
+    "https://playwright.dev/docs/inspector#open-playwright-inspector"
+  );
+  //   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
 
 test("Page PlayWRight Test", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test.only("Verified title of page", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
 });
