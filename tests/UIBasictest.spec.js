b53fefe111934191becc9a2a2d0ba53ff6dfb5a6
DucAnhNguyen <ducanhn1308@gmail.com>
2022-05-09 23:53:06 +0700

    create test get first and n product index

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -40,7 +40,78 @@ test.describe("Login page", () => {
     await page.locator("#signInBtn").click()
     //wait until this locator show up page
     console.log(await page.locator("[style*='block']").textContent()) //Incorrect username/password.
-    const locator_alert = await page.locator("[style*='block']").textContent()
+    const locator_alert = await page.locator("[style*='block']")
     await expect(locator_alert).toContainText("Incorrect")
+  })
+
+  test.only("Wrong username/password With variable ", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    //css selector
+    const username = page.locator("#username")
+    const passWord = page.locator("[type='password']")
+    const signInBtn = page.locator("#signInBtn")
+
+    await username.type("Bob Nguyen")
+    await passWord.type("123456")
+    await signInBtn.click()
+    //wait until this locator show up page
+    const locator_alert = await page.locator("[style*='block']")
+    await expect(locator_alert).toContainText("Incorrect")
+  })
+
+  test.only("Wrong username/password With FILL function ", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    //css selector
+    const useName = page.locator("#username")
+    const passWord = page.locator("[type='password']")
+    const signInBtn = page.locator("#signInBtn")
+
+    await useName.type("Bob Nguyen")
+    await passWord.type("123456")
+    await signInBtn.click()
+    //wait until this locator show up page
+    const locator_alert = await page.locator("[style*='block']")
+    await expect(locator_alert).toContainText("Incorrect")
+    //type -fill
+    await useName.fill("") // to clear all content in text field
+    await useName.fill("rahulshettyacademy") // to fill content into field
+    await passWord.fill("learning") // to fill content into field
+  })
+
+  test.only("Login successfully ", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    //css selector
+    const useName = page.locator("#username")
+    const passWord = page.locator("[type='password']")
+    const signInBtn = page.locator("#signInBtn")
+
+    //fill
+    await useName.fill("rahulshettyacademy") // to fill content into field
+    await passWord.fill("learning") // to fill content into field
+    await signInBtn.click()
+    await expect(page).toHaveTitle("ProtoCommerce")
+  })
+
+  test.only("Select childElement in DOM ", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    //css selector
+    const useName = page.locator("#username")
+    const passWord = page.locator("[type='password']")
+    const signInBtn = page.locator("#signInBtn")
+
+    //fill
+    await useName.fill("rahulshettyacademy") // to fill content into field
+    await passWord.fill("learning") // to fill content into field
+    await signInBtn.click()
+    await expect(page).toHaveTitle("ProtoCommerce")
+    //Get first child from parent
+    // Method 1
+    // const iphoneProduct = page.locator(".card-body a").nth(0)
+    // Method 2
+    const firstProduct = page.locator(".card-body a").first()
+    await expect(firstProduct).toContainText("iphone X")
+
+    const secondProduct = page.locator(".card-body a").nth(1)
+    await expect(secondProduct).toContainText("Samsung")
   })
 })
